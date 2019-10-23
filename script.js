c45e966dbf736192e4093e1077406a37d6e6ceb4
Melissa Robertson <melissalrobertson8@gmail.com>
2019-10-23 20:57:52 +0000

    Added verbose request, fixed list display

--- script.js
+++ script.js
@@ -5,13 +5,15 @@ var app = new Vue({
         typeSelection: "",
         apiKey: "k=348443-recommen-G0BZJ4EU",
         baseURL: "https://tastedive.com/api/similar?q=",
+        verbose: "verbose=1",
         cors: "https://cors-anywhere.herokuapp.com/",
-        types: ["band", "movie", "TV show", "podcast", "book", "author", "game"],
+        types: ["author", "band", "book", "game", "movie", "podcast", "TV show", ],
         recommendations: [],
     },
     methods: {
         getRecommendations() {
-            let url = this.cors + this.baseURL + this.typeSelection + ":" + this.modifiedInput + "," + this.apiKey;
+            let url = this.cors + this.baseURL + this.typeSelection + ":" + this.modifiedInput + "," + this.apiKey + "&" + this.verbose;
+            console.log(url)
             fetch(url)
                 .then((response) => {
                     return (response.json());
