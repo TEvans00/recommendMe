71046788f3f8e88221c9d9c8dddc293c041d915c
Tyla Merrill <tylamerrill@gmail.com>
2019-10-23 15:51:23 +0000

    Beginning to access API

--- script.js
+++ script.js
@@ -2,6 +2,21 @@ var app = new Vue({
     el: '#app',
     data: {
         typeSelection: "",
+        apiKey: "348443-recommen-G0BZJ4EU",
+        baseURL: "https://tastedive.com/api/similar?q=",
+        cors: "https://cors-anywhere.herokuapp.com/",
+        types: ["band", "movie", "TV show", "podcast", "books", "author", "game"],
+    },
+    methods: {
+        getRecommendations() {
+            let url = this.cors + this.baseURL + this.apiKey;
+            fetch(url)
+                .then((response) => {
+                    return (response.json());
+                })
+                .then((json) => {
+                    console.log(json);
+                });
+        }
     }
-
 });
