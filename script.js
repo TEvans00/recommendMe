186563d33dee8db39fe6f96e3d24821da7aad1b1
Tyla Merrill <tylamerrill@gmail.com>
2019-10-23 17:43:07 +0000

    Working for one item

--- script.js
+++ script.js
@@ -1,22 +1,31 @@
 var app = new Vue({
     el: '#app',
     data: {
+        inputSelection: "",
         typeSelection: "",
-        apiKey: "348443-recommen-G0BZJ4EU",
+        apiKey: "k=348443-recommen-G0BZJ4EU",
         baseURL: "https://tastedive.com/api/similar?q=",
         cors: "https://cors-anywhere.herokuapp.com/",
-        types: ["band", "movie", "TV show", "podcast", "books", "author", "game"],
+        types: ["band", "movie", "TV show", "podcast", "book", "author", "game"],
+        recommendations: [],
     },
     methods: {
         getRecommendations() {
-            let url = this.cors + this.baseURL + this.apiKey;
+            let url = this.cors + this.baseURL + this.typeSelection + ":" + this.modifiedInput + "," + this.apiKey;
             fetch(url)
                 .then((response) => {
                     return (response.json());
                 })
                 .then((json) => {
                     console.log(json);
+                    console.log(json.Similar.Results);
+                    this.recommendations = json.Similar.Results;
                 });
+        }
+    },
+    computed: {
+        modifiedInput: function() {
+            return this.inputSelection.replace(" ", "+");
         }
     }
 });
