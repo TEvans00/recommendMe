fcdb865509564f8c9bbdcd332ee289ab25d3d311
Tyla Merrill <tylamerrill@gmail.com>
2019-10-24 01:29:03 +0000

    Basic functionality finished

--- script.js
+++ script.js
@@ -1,19 +1,27 @@
 var app = new Vue({
     el: '#app',
     data: {
-        inputSelection: "",
-        typeSelection: "",
+        forms: [],
+        numForms: 0,
+        inputSelection: [],
+        typeSelection: [],
         apiKey: "k=348443-recommen-G0BZJ4EU",
         baseURL: "https://tastedive.com/api/similar?q=",
         verbose: "verbose=1",
         cors: "https://cors-anywhere.herokuapp.com/",
         types: ["author", "band", "book", "game", "movie", "podcast", "TV show", ],
         recommendations: [],
+        noRecommendations: false,
     },
     methods: {
         getRecommendations() {
-            let url = this.cors + this.baseURL + this.typeSelection + ":" + this.modifiedInput + "," + this.apiKey + "&" + this.verbose;
-            console.log(url)
+            let url = this.cors + this.baseURL;
+            console.log(this.inputSelection)
+            for (let i = 0; i < this.forms.length; i++) {
+                url += this.typeSelection[i] + ":" + this.inputSelection[i].replace(" ", "+") + ",";
+            }
+            url += this.apiKey + "&" + this.verbose;
+            console.log(url);
             fetch(url)
                 .then((response) => {
                     return (response.json());
@@ -22,15 +30,40 @@ var app = new Vue({
                     console.log(json);
                     console.log(json.Similar.Results);
                     this.recommendations = json.Similar.Results;
+                    if (this.recommendations.length === 0) {
+                        this.noRecommendations = true;
+                    }
                 })
                 .catch(error => {
                     console.log(error);
                 });
+        },
+        addForm() {
+            this.forms.push(this.numForms);
+            this.numForms++;
+            //this.forms.push("<form><input v-model='inputSelection'></input><select v-model='typeSelection'><option disabled value=''></option><option v-for='type in types'> {{ type }} </option></select></form>");
+        },
+        reset() {
+            this.numForms = 0;
+            this.forms = [];
+            this.typeSelection = [];
+            this.inputSelection = [];
+            this.addForm();
+            this.noRecommendations = false;
         }
     },
+    created: function() {
+        this.addForm();
+    },
     computed: {
-        modifiedInput: function() {
-            return this.inputSelection.replace(" ", "+");
-        }
+        /*modifiedInput: function() {
+            let newInput = [];
+            console.log(this.inputSelection[0]);
+            for (let i = 0; i < this.inputSelection.length(); i++) {
+                //console.log(this.inputSelection[i]);
+                //newInput.push(this.inputSelection[i].replace(" ", "+"));
+            }
+            return newInput;
+        }*/
     }
 });
