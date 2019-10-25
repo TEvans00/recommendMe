2dca7f92ad31b0e362b265f2b858a05c4d5c03f0
Tyla Merrill <tylamerrill@gmail.com>
2019-10-25 03:11:28 +0000

    Added some error catching

--- script.js
+++ script.js
@@ -12,14 +12,23 @@ var app = new Vue({
         types: ["author", "band", "book", "game", "movie", "podcast", "TV show", ],
         recommendations: [],
         noRecommendations: false,
+        limitExceded: false,
     },
     methods: {
         getRecommendations() {
+            this.limitExceded = false;
             this.noRecommendations = false;
             let url = this.cors + this.baseURL;
             console.log(this.inputSelection);
             for (let i = 0; i < this.forms.length; i++) {
-                url += this.typeSelection[i] + ":" + this.inputSelection[i].replace(" ", "+") + ",";
+                if (i < this.inputSelection.length) {
+                    if (i < this.typeSelection.length && this.typeSelection[i] != "") {
+                        url += this.typeSelection[i] + ":" + this.inputSelection[i].replace(" ", "+") + ",";
+                    }
+                    else {
+                        url += this.inputSelection[i].replace(" ", "+") + ",";
+                    }
+                }
             }
             url += this.apiKey + "&" + this.verbose;
             console.log(url);
@@ -37,6 +46,9 @@ var app = new Vue({
                 })
                 .catch(error => {
                     console.log(error);
+                    if (error == "Rate limit exceeded, try again later") {
+                        this.limitExceded = true;
+                    }
                 });
         },
         addForm() {
@@ -51,6 +63,7 @@ var app = new Vue({
             this.inputSelection = [];
             this.addForm();
             this.noRecommendations = false;
+            this.limitExceded = false;
         }
     },
     created: function() {
